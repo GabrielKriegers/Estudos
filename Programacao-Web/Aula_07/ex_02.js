8bfa60b40778e267519c87c58b1c30ed5a851531
GabrielKriegers <160859370+GabrielKriegers@users.noreply.github.com>
2024-08-17 15:29:06 -0300

    Add files via upload

---
+++ Programacao-Web/Aula_07/ex_02.js
@@ -0,0 +1,9 @@
+var n1 =  prompt("Digite um valor:");
+
+if (n1 > 0) {
+    document.write("O número é positivo");
+}else if (n1 < 0){
+    document.write("O número é negativo");
+}else {
+    document.write("O número é zero.");
+}
