1babf0335c07ffda325c82feed3da45a9242081d
alunoA106 <aluno@ifsc.edu.br>
2024-08-20 17:17:01 -0300

    Fim da aula 02

---
+++ PDM/Menu.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        int opcao = 0;
+        Scanner leitor = new Scanner(System.in);
+        while (true) {
+            System.out.print("Digite um número para calcular o quadrado ou digite 0 para sair: ");
+            opcao = leitor.nextInt();
+            if (opcao == 0) {
+                break;
+            }
+            System.out.println(square(opcao));
+        }
+    }
+    
+    public static int square(int n) {
+        return (n * n);
+    }
+
+}
