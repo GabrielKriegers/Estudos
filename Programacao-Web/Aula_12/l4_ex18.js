3bb9054270a741edab05f590558e1935b517d966
krieger1285 <gabrielkrieger1285@gmail.com>
2024-10-03 22:04:42 -0300

    Lista 04

---
+++ Programacao-Web/Aula_12/l4_ex18.js
@@ -0,0 +1,7 @@
+function nExtenso(){
+    const especiais = ["um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"];
+    const dezenas = ["vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"];
+    const centenas = ["cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"];
+
+    
+}
